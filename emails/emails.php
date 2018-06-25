ccf05b833dfabb37243f701653d80f49fed5cc6c
ahuntercatalano <alex.catalano2@gmail.com>
2018-06-25 16:33:22 -0400

    [emails] (180625, 4:33 p.m.)

---
+++ emails/emails.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>emails</title>
+    <link rel="stylesheet" href="css/emails.css">
+    <script src="scripts/email.js" charset="utf-8"></script>
+  </head>
+  <body>
+    <h1>AAC Emails</h1>
+    <h2>Leadership Team</h2>
+    <ul>
+      <li><script>email(alliCato)</script></li>
+      <li><script>email(joeSpe)</script></li>
+      <li><script>emailAd(lynnOSck)</script></li>
+      <li><script>emailAd(glenKpc)</script></li>
+      <li><script>emailAd(nichMzo)</script></li>
+      <li><script>emailAd(sarock)</script></li>
+      <li><script>emailAd(kathxrey)</script></li>
+      <li><script>emailAd(chrisrchmond)</script></li>
+      <li><script>emailAd(katsp)</script></li>
+      <li><script>emailAd(tmyung)</script></li>
+    </ul>
+
+    <h2>Professional Advisors</h2>
+    <ul>
+      <li><script>emailAd(bclman)</script></li>
+      <li><script>emailAd(egeer)</script></li>
+      <li><script>emailAd(chlyak)</script></li>
+      <li><script>emailAd(btnyHoov)</script></li>
+      <li><script>emailAd(bdonJdon)</script></li>
+      <li><script>emailAd(shaonkl)</script></li>
+      <li><script>emailAd(dykgn)</script></li>
+      <li><script>emailAd(bobbiKni)</script></li>
+      <li><script>emailAd(rtkng)</script></li>
+      <li><script>emailAd(lblcter)</script></li>
+      <li><script>emailAd(fllnLe)</script></li>
+      <li><script>emailAd(rhlnlTo)</script></li>
+      <li><script>emailAd(loopwers)</script></li>
+      <li><script>emailAd(ncolnd)</script></li>
+      <li><script>emailAd(ngelynd)</script></li>
+      <li><script>emailAd(claterbt)</script></li>
+    </ul>
+    <h2>CLAS Carreer Advising</h2>
+    <ul>
+      <li><script>email(andican)</script></li>
+    </ul>
+
+    <h2>Graduation Coordinator</h2>
+    <ul>
+      <li><script>emailAd(robnirt)</script></li>
+    </ul>
+
+    <h2>Lecturers</h2>
+    <ul>
+      <li><script>emailAd(rybran)</script></li>
+      <li><script>emailAd(brigrbs)</script></li>
+    </ul>
+
+    <h2>Advising Staff</h2>
+    <ul>
+      <li><script>emailAd(millba)</script></li>
+      <li><script>emailAd(alijda)</script></li>
+      <li><script>emailAd(beccers)</script></li>
+      <li><script>emailAd(shospan)</script></li>
+      <li><script>emailAd(kellmrie)</script></li>
+    </ul>
+
+    <h2>Advising Information Systems Staff</h2>
+    <ul>
+      <li><script>emailAd(alliCato)</script></li>
+      <li><script>emailAd(dashilds)</script></li>
+      <li><script>emailAd(sanwllam)</script></li>
+    </ul>
+
+
+    <h2>Graduate Student Assistants</h2>
+    <ul>
+      <li><script>emailAd(chelmatsn)</script></li>
+    </ul>
+
+    <footer>
+      <p>End.</p>
+    </footer>
+  </body>
+</html>
