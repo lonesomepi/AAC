80d9ec8c5c4a152d26bd84de7a1d36bcc8c77369
ahuntercatalano <alex.catalano2@gmail.com>
2019-01-24 08:00:13 -0500

    [folders, index] consolidate

---
+++ index/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>AAC Index</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+
+    <table>
+      <thead>
+        <tr>
+          <td>Items</td>
+          <td>Category</td>
+          <td>Action</td>
+        </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>AAC Calendar</td>
+        <td>calendar</td>
+      </tr>
+      <tr>
+        <td>Anderson Scholars</td>
+        <td>award</td>
+      </tr>
+      <tr>
+        <td>Beyond120</td>
+        <td>program</td>
+      </tr>
+      <tr>
+        <td>Blogs</td>
+        <td>blog</td>
+      </tr>
+      <tr>
+        <td>CLAS Ambassadors</td>
+        <td>role</td>
+      </tr>
+      <tr>
+        <td>CLAS IT</td>
+        <td>IT</td>
+      </tr>
+      <tr>
+        <td>CLAS Majors (?)</td>
+        <td>/majors/</td>
+        <td>steal this site</td>
+      </tr>
+      <tr>
+        <td>CLAS WordPress Access</td>
+        <td>role</td>
+      </tr>
+      <tr>
+        <td>Committees</td>
+        <td>/committees/</td>
+      </tr>
+      <tr>
+        <td>Communications Submission</td></tr>
+      <tr>
+        <td>Data Security Administrator</td></tr>
+      <tr>
+        <td>Dean’s List</td></tr>
+      <tr>
+        <td>Dean’s Medal</td></tr>
+      <tr>
+        <td>Dean’s Medal Application</td></tr>
+      <tr>
+        <td>Dean’s Newsletter</td></tr>
+      <tr>
+        <td>Emails</td></tr>
+      <tr>
+        <td>Evaluation</td></tr>
+      <tr>
+        <td>Evaluation</td></tr>
+      <tr>
+        <td>Evaluation Emails</td></tr>
+      <tr>
+        <td>Evaluation Reports</td></tr>
+      <tr>
+        <td>FaceBook</td></tr>
+      <tr>
+        <td>Hardware</td></tr>
+      <tr>
+        <td>Headcount</td></tr>
+      <tr>
+        <td>Lists</td></tr>
+      <tr>
+        <td>Lobby Management</td></tr>
+      <tr>
+        <td>Lobby Sign In</td></tr>
+      <tr>
+        <td>Manual</td></tr>
+      <tr>
+        <td>Minors</td></tr>
+      <tr>
+        <td>MySQL Hosting</td></tr>
+      <tr>
+        <td>PeopleSoft Roles</td></tr>
+      <tr>
+        <td>Pre-Health Calendar</td>
+        <td>calendar</td>
+      </tr>
+      <tr>
+        <td>Pre-Health Postbac</td></tr>
+      <tr>
+        <td>Pre-Law Calendar</td>
+        <td>calendar</td>
+      </tr>
+      <tr>
+        <td>Prep Sheets</td></tr>
+      <tr>
+        <td>Preview Advisor Application</td></tr>
+      <tr>
+        <td>Preview Advisor Scheduling</td></tr>
+      <tr>
+        <td>Preview Prep Admin</td></tr>
+      <tr>
+        <td>Preview Prep site</td></tr>
+      <tr>
+        <td>Probation</td></tr>
+      <tr>
+        <td>Special Projects</td></tr>
+      <tr>
+        <td>TSS</td></tr>
+      <tr>
+        <td>Twitter</td></tr>
+      <tr>
+        <td>UF Online</td></tr>
+      <tr>
+        <td>UF Online Dashboard</td></tr>
+      <tr>
+        <td>Web Hosting</td></tr>
+      <tr>
+        <td>Website</td></tr>
+      <tr>
+        <td>WordPress</td></tr>
+      <tr>
+        <td>Workshops</td></tr>
+      <tr>
+        <td>Youtube</td>
+      </tr>
+      </tbody>
+    </table>
+
+</body>
+</html>
